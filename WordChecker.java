77594d99699cf6cbe7c397309fc4a2b4063d86e5
BT_1N3_20 <Vincentc270@nycstudents.net>
2024-10-11 12:35:04 -0400

    w

---
+++ WordChecker.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class WordChecker {
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = list;
+        wordList = new ArrayList<String>();
+
+    }
+}
--- main1.java
+++ main1.java
@@ -1,5 +1,7 @@
 public class main1 {
     public static void main(String[] args){
         WordChecker w = new WordChecker();
+        ArrayList<String> words = new ArrayList<String>();
     }
+
 }

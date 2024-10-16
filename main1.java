693c7367ec56d9d136e3db50b680ff2931cee60a
c4thyc <cathyc22@nycstudents.net>
2024-10-16 12:13:15 -0400

    Update main1.java + added test cases

--- main1.java
+++ main1.java
@@ -1,7 +1,14 @@
-public class main1 {
-    public static void main(String[] args){
+public class Main 
+{
+    public static void main(String[] args)
+    {
         WordChecker w = new WordChecker();
         ArrayList<String> words = new ArrayList<String>();
+        WordChecker x = new WordChecker(words);
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
     }
 
 }

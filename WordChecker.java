f1d26edcae9d64af6b5b204c1721bc9e42237c24
c4thyc <cathyc22@nycstudents.net>
2024-10-24 12:06:29 -0400

    Update WordChecker.java

--- WordChecker.java
+++ WordChecker.java
@@ -1,11 +1,45 @@
 import java.util.ArrayList;
-public class WordChecker {
-    public WordChecker(ArrayList<String> list)
-    {
-        wordList = list;
+    public class WordChecker {
+        /** Initialized in the constructor and contains no null elements */
+        private ArrayList<String> wordList;
+
+        public WordChecker()
+        {
+            wordList = list;  
+        }
+
+        public WordChecker(ArrayList<String> list)
+        {
+            wordList = new ArrayList<String>();
+        }
+        /**
+         * Returns true if each element of wordList (except the first) contains the
+         * previous
+         * element as a substring and returns false otherwise, as described in part (a)
+         * Precondition: wordList contains at least two elements.
+         * Postcondition: wordList is unchanged.
+         */
+        public boolean isWordChain()
+        {
+            for (int i = 1; i < wordList.size(); i++)
+            {
+                String after = wordList.get(i);
+                String before = wordList.get(i - 1);
+                if (after.indexOf(before) < 0) return false;
+            }
+        }
+        {
+            /* to be implemented in part (a) */
+        }
+        /**
+         * Returns an ArrayList<String> based on strings from wordList that start
+         * with target, as described in part (b). Each element of the returned ArrayList
+         * has had
+         * the initial occurrence of target removed.
+         * Postconditions: wordList is unchanged.
+         * Items appear in the returned list in the same order as they appear in
+         * wordList.
+         */
+        // public ArrayList<String> createList(String target) {
+            /* to be implemented in part (b) } */ 
     }
-    public WordChecker()
-    {
-        wordlist = new ArrayList<String>();
-    }
-}

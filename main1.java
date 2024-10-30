2a0a34483fdc85415f609c3fff5c30752e9c0af7
c4thyc <cathyc22@nycstudents.net>
2024-10-30 12:29:19 -0400

    Update main1.java

--- main1.java
+++ main1.java
@@ -9,9 +9,26 @@ public class Main
         words.add("band");
         words.add("band");
         words.add("abandon");
-        System.out.print(words);
-        WordChecker x = new WordChecker(words);
-        System.out.println(x.isWordChain());
+        // System.out.print(words);
+        //WordChecker x = new WordChecker(words);
+        // System.out.println(x.isWordChain());
+        ArrayList<String> words2 = new ArrayList<String>();
+        words2.add("to");
+        words2.add("too");
+        words2.add("stool");
+        words2.add("tools");
+        WordChecker y = new WordChecker(words2);
+        System.out.println(y.isWordChain());
+        ArrayList<String> test1 = new ArrayList<String>();
+        test1.add("catch");
+        test1.add("bobcat");
+        test1.add("catchacat");
+        test1.add("cat");
+        test1.add("at");
+        WordChecker x = new WordChecker(test1);
+        System.out.println(x.createList("cat"));
+        System.out.println(x.createList("catch"));
+        System.out.println(x.createList("dog"));
     }
 
 }
